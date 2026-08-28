sipeed/NanoKVM

Add a compressed swap (zram) toggle and status row to the settings UI
zram works on this board, but only as a manual install: build the kernel
modules, then copy an init script to /etc/init.d by hand. The web UI reports
nothing about it, so an operator cannot tell whether compressed swap runs, or
how close it is to its memory cap. This adds one row to Settings > Device >
Advanced that reports the state and switches it on or off.

The row reports three independent booleans rather than one flag. Available is
whether the kernel modules are present, enabled is whether the init script is
installed, and active is whether /dev/zram0 is in /proc/swaps. "Enabled but
not active" is the state that matters most, and a single flag would render it
as plain "off" -- inviting the operator to toggle it on and watch nothing
happen. The row shows it in amber instead.

The modules stay a manual step. zram and zsmalloc are built out of tree
against the stock kernel and pinned to its vermagic, so shipping them in the
install package would mean reshipping them whenever the kernel moves.
tools/zram/build-modules.sh builds both. Without them the toggle is disabled
and says so.

S01zram lives in kvmapp/system/init.d so the package carries it and the server
has a source to copy from. It is deliberately not added to the copy list in
system_init.cpp: that list is hard-coded C++, and a name added there needs a
MaixCDK rebuild plus a kvm_system redeploy on every device. The server owns
the copy, the way S98tailscaled already treats presence in /etc/init.d as the
installed marker. A start that produces no device rolls the install back, so
enabled never claims a reboot will help.

Three defects surfaced while wiring this up, and all three are fixed here.

disableSwap ran `swapoff -a`, which stops every swap device. Any change to the
swap file control therefore stopped zram as a side effect, which is not what a
control labelled "swap file size" should do. It now names the file, and skips
the call en

server/router/vm.go
@@ -49,6 +49,9 @@ func vmRouter(r *gin.Engine) {
 	api.GET("/vm/swap", service.GetSwap)  // get swap file size
 	api.POST("/vm/swap", service.SetSwap) // set swap file size
 
+	api.GET("/vm/zram", service.GetZram)  // get compressed swap state
+	api.POST("/vm/zram", service.SetZram) // enable or disable compressed swap
+
 	api.GET("/vm/mouse-jiggler", service.GetMouseJiggler)   // get mouse jiggler
 	api.POST("/vm/mouse-jiggler/", service.SetMouseJiggler) // set mouse jiggler
 

yuzi-co/IronKVM

server/router/vm.go
@@ -58,6 +58,9 @@ func vmRouter(r *gin.Engine) {
 	admin.GET("/vm/swap", service.GetSwap)  // get swap file size
 	admin.POST("/vm/swap", service.SetSwap) // set swap file size
 
+	admin.GET("/vm/zram", service.GetZram)  // get compressed swap state
+	admin.POST("/vm/zram", service.SetZram) // enable or disable compressed swap
+
 	admin.GET("/vm/mouse-jiggler", service.GetMouseJiggler)   // get mouse jiggler
 	admin.POST("/vm/mouse-jiggler/", service.SetMouseJiggler) // set mouse jiggler
 